aa5c51edfc4ea91d912876cb1d6bab624fc4f04c
yimmm10 <yimaries10@gmail.com>
2026-05-20 11:50:01 +0700

    Initial commit with Cosmic Tarot UI enhancements

---
+++ images.js
@@ -0,0 +1,26 @@
+// Rider-Waite-Smith Tarot Card Images (Public Domain, 1909)
+// Source: Wikimedia Commons
+const TAROT_IMAGES = [
+    "assets/tarot/card_00.jpg",
+    "assets/tarot/card_01.jpg",
+    "assets/tarot/card_02.jpg",
+    "assets/tarot/card_03.jpg",
+    "assets/tarot/card_04.jpg",
+    "assets/tarot/card_05.jpg",
+    "assets/tarot/card_06.jpg",
+    "assets/tarot/card_07.jpg",
+    "assets/tarot/card_08.jpg",
+    "assets/tarot/card_09.jpg",
+    "assets/tarot/card_10.jpg",
+    "assets/tarot/card_11.jpg",
+    "assets/tarot/card_12.jpg",
+    "assets/tarot/card_13.jpg",
+    "assets/tarot/card_14.jpg",
+    "assets/tarot/card_15.jpg",
+    "assets/tarot/card_16.jpg",
+    "assets/tarot/card_17.jpg",
+    "assets/tarot/card_18.jpg",
+    "assets/tarot/card_19.jpg",
+    "assets/tarot/card_20.jpg",
+    "assets/tarot/card_21.jpg"
+];
